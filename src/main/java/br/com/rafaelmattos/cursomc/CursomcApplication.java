4bca05b60a42737391dea1106a43a60ffa1d57eb
Rafael Mattos <rafaelrj@live.com>
2021-07-08 20:32:17 -0300

    Criando operacao de instanciacao

--- src/main/java/br/com/rafaelmattos/cursomc/CursomcApplication.java
+++ src/main/java/br/com/rafaelmattos/cursomc/CursomcApplication.java
@@ -1,13 +1,38 @@
 package br.com.rafaelmattos.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import br.com.rafaelmattos.cursomc.domain.Categoria;
+import br.com.rafaelmattos.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+//CommandLineRunner -> Implementar um metodo auxiliar executar uma ação qdo a aplicação iniciar.
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Cama mesa e banho");
+		Categoria cat4 = new Categoria(null, "Eletrônicos");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
+	
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5 ,cat6, cat7));
 	}
 
 }
