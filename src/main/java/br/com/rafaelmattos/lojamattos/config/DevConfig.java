6c032f1954672e0ac7fbb6e466f6e57ff7347dfa
Rafael Mattos <rafaelrj@live.com>
2021-07-16 01:02:08 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/br/com/rafaelmattos/lojamattos/config/DevConfig.java
@@ -0,0 +1,35 @@
+package br.com.rafaelmattos.lojamattos.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.rafaelmattos.lojamattos.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	//estrategia de geração de banco de dados
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		//instanciar a classe de serviços para teste
+		//se a estrategia nao for igual a create
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+}
