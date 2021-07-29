e7e845317313e90788382a4a4de2d98f58f71f83
Rafael Mattos <rafaelrj@live.com>
2021-07-28 22:17:30 -0300

    Implementando autorização

--- src/main/java/br/com/rafaelmattos/lojamattos/config/SecurityConfig.java
+++ src/main/java/br/com/rafaelmattos/lojamattos/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import br.com.rafaelmattos.lojamattos.security.JWTAuthenticationFilter;
+import br.com.rafaelmattos.lojamattos.security.JWTAuthorizationFilter;
 import br.com.rafaelmattos.lojamattos.security.JWTUtil;
 
 @Configuration
@@ -79,7 +80,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MARCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
-//		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		
 		//backend nao vai criar sessão de usuario
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
--- src/main/java/br/com/rafaelmattos/lojamattos/security/JWTAuthenticationFilter.java
+++ src/main/java/br/com/rafaelmattos/lojamattos/security/JWTAuthenticationFilter.java
@@ -2,6 +2,7 @@ package br.com.rafaelmattos.lojamattos.security;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -12,6 +13,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -28,7 +30,8 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     
 	//injeção de dependencias pelo construtor
     public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
-//    	setAuthenticationFailureHandler(new JWTAuthenticationFailureHandler());
+    	//acrescentar uma implementação de AuthenticationFailureHandler e injetá-la em JWTAuthenticationFilter para injetar o codigo 401
+    	setAuthenticationFailureHandler(new JWTAuthenticationFailureHandler());
         this.authenticationManager = authenticationManager;
         this.jwtUtil = jwtUtil;
     }
@@ -64,23 +67,25 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         //res.addHeader("access-control-expose-headers", "Authorization");
 	}
 	
-//	private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
-//		 
-//        @Override
-//        public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
-//                throws IOException, ServletException {
-//            response.setStatus(401);
-//            response.setContentType("application/json"); 
-//            response.getWriter().append(json());
-//        }
-//        
-//        private String json() {
-//            long date = new Date().getTime();
-//            return "{\"timestamp\": " + date + ", "
-//                + "\"status\": 401, "
-//                + "\"error\": \"Não autorizado\", "
-//                + "\"message\": \"Email ou senha inválidos\", "
-//                + "\"path\": \"/login\"}";
-//        }
-//    }
+	
+	//acrescentar uma implementação de AuthenticationFailureHandler e injetá-la em JWTAuthenticationFilter para injetar o codigo 401
+	private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
+		 
+        @Override
+        public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
+                throws IOException, ServletException {
+            response.setStatus(401);
+            response.setContentType("application/json"); 
+            response.getWriter().append(json());
+        }
+        
+        private String json() {
+            long date = new Date().getTime();
+            return "{\"timestamp\": " + date + ", "
+                + "\"status\": 401, "
+                + "\"error\": \"Não autorizado\", "
+                + "\"message\": \"Email ou senha inválidos\", "
+                + "\"path\": \"/login\"}";
+        }
+    }
 }
