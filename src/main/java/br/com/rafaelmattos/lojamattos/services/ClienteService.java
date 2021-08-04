9662084225abb0216478a51bad12963a254f9bd2
Rafael Mattos <rafaelrj@live.com>
2021-08-04 17:15:48 -0300

    ajustando tamanho da imagem

--- src/main/java/br/com/rafaelmattos/lojamattos/services/ClienteService.java
+++ src/main/java/br/com/rafaelmattos/lojamattos/services/ClienteService.java
@@ -47,9 +47,12 @@ public class ClienteService {
 	
 	@Autowired
 	private ImageService imageService;
-	
+		
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
+	
+	@Value("${img.profile.size}")
+	private Integer size;
 
 	// buscar no banco de dados com base no id
 	public Cliente find(Integer id) {
@@ -143,6 +146,9 @@ public class ClienteService {
 		}
 		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		
 		String fileName = prefix + user.getId() + ".jpg";
 		
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
--- src/main/java/br/com/rafaelmattos/lojamattos/services/ImageService.java
+++ src/main/java/br/com/rafaelmattos/lojamattos/services/ImageService.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FilenameUtils;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -54,6 +55,22 @@ public class ImageService {
 		} catch (IOException e) {
 			throw new FileException("Erro ao ler arquivo");
 		}
+	}
+	
+	//deixar a imagem quadrada
+	public BufferedImage cropSquare(BufferedImage sourceImg) {
+		int min = (sourceImg.getHeight() <= sourceImg.getWidth()) ? sourceImg.getHeight() : sourceImg.getWidth();
+		return Scalr.crop(
+			sourceImg, 
+			(sourceImg.getWidth()/2) - (min/2), 
+			(sourceImg.getHeight()/2) - (min/2), 
+			min, 
+			min);		
+	}
+	
+	//redirecionar a imagem
+	public BufferedImage resize(BufferedImage sourceImg, int size) {
+		return Scalr.resize(sourceImg, Scalr.Method.ULTRA_QUALITY, size);
 	}
 	
 }
