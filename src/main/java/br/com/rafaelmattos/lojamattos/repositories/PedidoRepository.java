95c38c515669508f8f9b56032d52ec095735cbb5
Rafael Mattos <rafaelrj@live.com>
2021-08-03 21:28:56 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/br/com/rafaelmattos/lojamattos/repositories/PedidoRepository.java
+++ src/main/java/br/com/rafaelmattos/lojamattos/repositories/PedidoRepository.java
@@ -1,8 +1,12 @@
 package br.com.rafaelmattos.lojamattos.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.rafaelmattos.lojamattos.domain.Cliente;
 import br.com.rafaelmattos.lojamattos.domain.Pedido;
 
 @Repository
@@ -10,4 +14,9 @@ import br.com.rafaelmattos.lojamattos.domain.Pedido;
 //herdar a classe por JpaRepository, atributo identificador
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
 
+	//reduzir "lock"
+	@Transactional(readOnly = true)
+	//buscar pedidos por cliente, fazendo paginação
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
+	
 }
--- src/main/java/br/com/rafaelmattos/lojamattos/services/PedidoService.java
+++ src/main/java/br/com/rafaelmattos/lojamattos/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.rafaelmattos.lojamattos.domain.Cliente;
 import br.com.rafaelmattos.lojamattos.domain.ItemPedido;
 import br.com.rafaelmattos.lojamattos.domain.PagamentoComBoleto;
 import br.com.rafaelmattos.lojamattos.domain.Pedido;
@@ -14,6 +18,8 @@ import br.com.rafaelmattos.lojamattos.domain.enums.EstadoPagamento;
 import br.com.rafaelmattos.lojamattos.repositories.ItemPedidoRepository;
 import br.com.rafaelmattos.lojamattos.repositories.PagamentoRepository;
 import br.com.rafaelmattos.lojamattos.repositories.PedidoRepository;
+import br.com.rafaelmattos.lojamattos.security.UserSS;
+import br.com.rafaelmattos.lojamattos.services.exceptions.AuthorizationException;
 import br.com.rafaelmattos.lojamattos.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -85,6 +91,18 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		
+		//obter o usuario logado
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 	
 }
